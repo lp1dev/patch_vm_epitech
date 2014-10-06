2448f375d9b15a58976d836473db87d5bbd9da21
Jeremie Amsellem <mail@lupin.me>
2014-10-06 20:41:01 +0200

    added verbose

--- patch.sh
+++ patch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 if [ $# == 1 ]
 then
+    echo "Copie du script sur la VM exam"
     scp res/network-wait root@$1:/usr/sbin/network-wait
+    echo "Chmod +x sur le script"
     ssh root@$1 chmod +x /usr/sbin/network-wait
+    echo "Patch installé"
+    echo "Bon exam !"
 else
     echo "Usage : " $0 "ip_vm"
 fi
--- patch_rsync.sh
+++ patch_rsync.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 if [ $# == 1 ]
 then
+    echo "Rsync du script manquant sur la VM"
     rsync --chmod=u+rwx,g+rwx,o+rwx res/network-wait root@$1:/usr/sbin/network-wait
+    echo "Installation terminée"
+    echo "Bon exam"
 else
     echo "Usage : " $0 "ip_vm"
 fi
